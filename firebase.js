1668cff7a942bcdccaa65256fccf9226ba107181
Yusuf Harb <59343058+yusufharb@users.noreply.github.com>
2026-01-20 00:47:50 +0200

    Replace Firebase config with new credentials
    Updated Firebase configuration with new credentials.

--- firebase.js
+++ firebase.js
@@ -4,12 +4,12 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBOr3NRGjrEnCwDqqR-2feqD_9MEDSp3p8",
-  authDomain: "elgohary-88978.firebaseapp.com",
-  projectId: "elgohary-88978",
-  storageBucket: "elgohary-88978.firebasestorage.app",
-  messagingSenderId: "378343151130",
-  appId: "1:378343151130:web:ed62b8dfaf4dbffd6563a3"
+  apiKey: "XXXXXXXXXXXXXXXXXXXXXXXX",
+  authDomain: "XXXXXXXXXXXXXX.firebaseapp.com",
+  projectId: "XXXXXXXXXXXXXXXX",
+  storageBucket: "XXXXXXXXXXXXXXXXXXX",
+  messagingSenderId: "0000000000xxx",
+  appId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 };
 
 const app = initializeApp(firebaseConfig);
